c7b819d0482cfa60dc9615cb4090f9d7cc9c9799
anhvu21 <Vuphamducanh2000@gmail.com>
2023-07-29 16:33:14 -0400

    e

--- backend/database.sql
+++ backend/database.sql
@@ -10,3 +10,13 @@ CREATE TABLE messages (
   timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   FOREIGN KEY (shared_post_id) REFERENCES posts (id)
 );
+
+CREATE TABLE Users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    bio VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP,
+    group_id  int
+);
