6564428205c96dfad40a1024fe728298e70ee894
anhvu21 <Vuphamducanh2000@gmail.com>
2023-07-30 18:54:10 -0400

    e

--- backend/database.sql
+++ backend/database.sql
@@ -20,3 +20,37 @@ CREATE TABLE Users (
     updated_at TIMESTAMP,
     group_id  int
 );
+
+CREATE TABLE Posts (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    content TEXT,
+    up_down VARCHAR(255),
+    group_id INTEGER NOT NULL,
+    image_url VARCHAR(255),
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE CASCADE
+);
+CREATE TABLE Comments (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    post_id INTEGER NOT NULL,
+    content TEXT,
+    group_id INTEGER NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE CASCADE,
+    FOREIGN KEY (post_id) REFERENCES Posts(id) ON DELETE CASCADE
+);
+CREATE TABLE Likes (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    post_id INTEGER,
+    comment_id INTEGER,
+    group_id INTEGER NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE CASCADE,
+    FOREIGN KEY (post_id) REFERENCES Posts(id) ON DELETE SET NULL,
+    FOREIGN KEY (comment_id) REFERENCES Comments(id) ON DELETE SET NULL
+);
